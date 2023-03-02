ed1957104768ef456b803a358a051146b4c40b81
Picci <enrico.piccinin@gmail.com>
2023-03-02 15:33:01 +0100

    code for v2.0.0 requires Module B and Module C

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/EnricoPicci/go-class-dep-mgmt-mod-a
 
 go 1.20
 
-require github.com/EnricoPicci/go-class-dep-mgmt-mod-b v1.0.1-0.20230301172015-69a64ee33d8a
+require github.com/EnricoPicci/go-class-dep-mgmt-mod-b v1.1.0
+
+require (
+	github.com/EnricoPicci/go-class-dep-mgmt-mod-c v1.1.0 // indirect
+	github.com/EnricoPicci/go-class-dep-mgmt-mod-d v1.1.0 // indirect
+)
--- main/main.go
+++ main/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/EnricoPicci/go-class-dep-mgmt-mod-b/pkgmodb1"
+	"github.com/EnricoPicci/go-class-dep-mgmt-mod-b/pkgmodb2"
 )
 
 func main() {
-	fmt.Println("I am the main program in Module A that calls a function of Module B")
-	fmt.Println("The function of Module B does not call any other function from external modules")
-	pkgmodb1.ApiThatDoesNotCallExternalApis()
+	fmt.Println("I am the main program in Module A that calls a function of Module B that calls a function of Module C")
+	fmt.Println("The function of Module B calls a function from the external module Module C")
+	pkgmodb2.ApiThatCallsModuleCApiThatCallRemainsInteral()
 }
